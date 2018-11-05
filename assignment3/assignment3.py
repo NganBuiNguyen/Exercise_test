3bdb6add81524c033219e8b1d47ebf3b7c5cc81a
NganBui <nganbui180995@gmail.com>
2018-11-06 01:41:47 +0700

    NganBui: Update assignment3.py to add function num_of_clicked_categories

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -3,7 +3,7 @@ import json
 import sys
 import glob
 
-files = []
+
 """
 This is function to get list file json in activities directory
 input: location of activities directory
@@ -11,15 +11,31 @@ input: location of activities directory
 def get_list_file(location):
     for file_name in glob.glob(location + "/*.json"):
         yield open(os.path.join(location, file_name))
-
+"""
+This is function to load categories file
+"""
+def get_categories_file():
+    return json.load(open("/home/nganbui/Documents/Job/assignment3/items_categories.json","r"))
+     
+"""
+This is funtion to get activity clicked and get category 
+"""
+def num_of_clicked_categories(data):
+    list_categories = list()
+    name = ''
+    if data['activity'] == 'clicked':
+        for item in get_categories_file():
+            if data['productId'] == item['item_id']:
+                print(item['category'])
+        return True
 """
 This is function to read content json file
 """
 def read_json_file(location_file_json):
-    list_file = list()
+    data_json = list()
     for file in get_list_file(location_file_json):
         data_json = json.load(file)
-    
+        print(len(list(filter(num_of_clicked_categories, data_json))))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
