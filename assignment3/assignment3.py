97c34e7e9481d95b6750462d06ad23cee0145154
NganBui <nganbui180995@gmail.com>
2018-11-08 02:13:12 +0700

    NganBui: Update code to built format json result for number of unique users clicked on any items and update number of clicked

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -5,45 +5,82 @@ import glob
 import re
 from collections import Counter
 
+
+location_file_categories = "/home/nganbui/Documents/Job/exercise/Exercise_test/assignment3/items_categories.json"
 """
 This is function to get list file json in activities directory
 input: location of activities directory
 """
 def get_list_file(location):
     for file_name in glob.glob(location + "/*.json"):
-        yield open(file_name)
-"""
-This is function to load categories file
-"""
-def get_categories_file(list_click, location_file_categories):
-    list_category_click = list()
-    data_category = json.load(open(location_file_categories,"r"))
-    for item_productId in list_click:
-        for item in data_category:
-            if item_productId['productId'] == item['item_id']:
-                list_category_click.append(item['category'])
-    return list_category_click                 
-     
+        yield open(file_name)                
+
 """
 This is funtion to get activity clicked and get category 
 """
-def num_of_clicked_categories(data):
+def num_of_clicked(data):
+    data_category = json.load(open(location_file_categories,"r"))
     name = ''
     if data['activity'] == 'clicked':
+        for item in data_category:
+            if data['productId'] == item['item_id']:
+                data['productId'] = item['category']
         return True
+
+def count_categorie(list_click):
+    list_category = list()
+    for item in list_click:
+        list_category.append(item['productId'])    
+    return list_category
+    
+def num_of_click_by_categorie(list_click):
+    list_result = list()
+    dict_json_result_click = dict()
+    for item in list_click:
+        dict_json_result_click['date'] = item['date']
+        dict_json_result_click['category'] = item['productId']
+        list_count = count_categorie(list_click)
+        dict_json_result_click['clicks'] = list_count.count(item['productId'])
+    list_result.append(dict_json_result_click)
+    print(list_result)
+
+def count_categorie_user(product_id, list_click):
+    list_user = list()
+    for item in list_click:
+        if product_id == item['productId']:
+            list_user.append(item['userId'])
+    return list_user
+
+
+def num_of_users_by_categorie(list_click):
+    list_result = list()
+    dict_json_result_click = dict()
+    for item in list_click:
+        dict_json_result_click['date'] = item['date']
+        dict_json_result_click['category'] = item['productId']
+        dict_json_result_click['users'] = item['userId']
+        list_user = count_categorie_user(item['productId'], list_click)
+        dict_json_result_click['num_users'] = list_user.count(item['userId'])
+    list_result.append(dict_json_result_click)
+    print(list_result)
+
+
 """
 This is function to read content json file
 """
-def read_json_file(location_file_json, location_file_categories):
+def read_json_file(location_file_json, choose):
     data_json = list()
-    dict_json_result = dict()
+    dict_json_result_click = dict()
+    dict_json_result_user = dict()
     for file in get_list_file(location_file_json):
         date_tiem = str(file.name).split('/')[-1].split('.')[0]
         data_json = json.load(file)
-        list_click = list(filter(num_of_clicked_categories, data_json))
-        list_category = get_categories_file(list_click, location_file_categories)
-        dict_json_result[date_tiem] = dict(Counter(list_category))
-    print(json.dumps(dict_json_result, sort_keys=True, indent=4))
+        list_click = list(filter(num_of_clicked, data_json))
+        if choose == 0:
+            num_of_click_by_categorie(list_click)
+        elif choose == 1:
+            num_of_users_by_categorie(list_click)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -51,7 +88,7 @@ if __name__ == "__main__":
         sys.exit()
 
     location_file_json = sys.argv[1]
-    location_file_categories = sys.argv[2]
+    choose = int(sys.argv[2])
 
-    read_json_file(location_file_json, location_file_categories)
+    read_json_file(location_file_json, choose)
 
